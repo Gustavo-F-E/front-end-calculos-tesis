895475a98fad36df3c35a2d61efdd1fb06885171
Gustavo-F-E <112873809+Gustavo-F-E@users.noreply.github.com>
2024-04-23 16:59:07 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//Importaciones de los módulos de prism.js
+import './prism.js';
+import './prism-python.min.js';
+import './prism-javascript.min.js';
+import './prism-line-highlight.js';
+//import '../css/prism-vsc-dark-plus.css'
+// Importa más lenguajes si es necesario
+
+//Importe de modulos personalizados
+
+const d = document;
+
+//Otras Funciones
+
+
+Prism.highlightAll();
+Prism.plugins.lineHighlight.applyHighlight();
+
+
+
